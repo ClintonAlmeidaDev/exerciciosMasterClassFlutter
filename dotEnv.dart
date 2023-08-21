4c6a095241030915afe74062b2357d110af643ab
Clinton <clinton.almeida10@gmail.com>
2023-08-21 13:24:48 -0300

    Subindo dotEnv com bug que não carrega valores diretamente pela classe

---
+++ dotEnv.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+import 'dart:io';
+
+class LoadEnv {
+  final String nomeArquivo;
+
+  LoadEnv(this.nomeArquivo);
+
+  Map<String, dynamic> _valores = {};
+
+  Map get valores {
+    carregaEnv();
+    return _valores;
+  }
+
+  void lerArquivo() {
+    File(this.nomeArquivo).readAsString().then((String content) {
+      print(content);
+    });
+  }
+
+  Future<void> carregaEnv() async {
+    final file = File(this.nomeArquivo);
+
+    Stream<String> lines = file
+        .openRead()
+        .transform(utf8.decoder) // Decode bytes to UTF-8.
+        .transform(LineSplitter()); // Convert stream to individual lines.
+    try {
+      await for (var line in lines) {
+        separaChaveValor(line, this._valores);
+      }
+      print('File is now closed.');
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  void separaChaveValor(String line, Map<String, dynamic> valores) {
+    List<String> partes = line.split('=');
+    separaPorTipo(partes, valores);
+  }
+
+  void separaPorTipo(List<String> partes, Map<String, dynamic> valores) {
+    if (partes.length == 2) {
+      String chave = partes[0];
+      String valor = partes[1];
+
+      try {
+        int valorInt = int.parse(valor);
+        // print("INTEIRO");
+        valores.addAll({chave: valorInt});
+      } catch (e1) {
+        try {
+          double valorDouble = double.parse(valor);
+          // print('DOUBLE');
+          valores.addAll({chave: valorDouble});
+        } catch (e2) {
+          try {
+            bool valorBolean = true;
+            if (valor.toLowerCase() == 'true') {
+              valorBolean = true;
+              valores.addAll({chave: valorBolean});
+              // print("BOOL - TRUE");
+            } else if (valor.toLowerCase() == 'false') {
+              valorBolean = false;
+              valores.addAll({chave: valorBolean});
+              // print("BOOL - FALSE");
+            } else {
+              // print("STRING");
+              valores.addAll({chave: valor});
+            }
+          } catch (e3) {}
+        }
+      }
+    }
+  }
+}
+
+Future<void> main() async {
+  var env = LoadEnv('.env');
+  await env.carregaEnv();
+  print(env.valores["ambiente"]);
+}
